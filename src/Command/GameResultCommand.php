643fab9cd8986483e1692a2845eec275b2304e38
Shailesh Jamanapara <32861894+shaileshatvectsolutions@users.noreply.github.com>
2020-12-18 13:40:37 +0530

    Update GameResultCommand.php
    Implemented command sequence as per provided assignment

--- src/Command/GameResultCommand.php
+++ src/Command/GameResultCommand.php
@@ -8,35 +8,88 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Helper;
+use Symfony\Component\Console\Helper\FormatterHelper;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Question\Question;
 
 class GameResultCommand extends Command
 {
     protected static $defaultName = 'gameResult';
+    protected $team = array();
+    protected $is_valid = false;
+    protected $error;
+    protected $result = 'Win';
 
     protected function configure()
     {
         $this
-            ->setDescription('Add a short description for your command')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
+        ->setDescription('Eastern Enterprise – Assignment');
+    }
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $formatter = $this->getHelper('formatter');
+        $helper = $this->getHelper('question');
+        $question =  new Question('Enter A Teams players:');
+        $strTeamA = $helper->ask($input, $output, $question);
+        $this->validateArgument($strTeamA, 'A');
+        if($this->is_valid == false){
+            $errorMessages = array('Error!', $this->error );
+            $formattedBlock = $formatter->formatBlock($errorMessages, 'error');
+            $output->writeln($formattedBlock);
+            return Command::SUCCESS;
+        }
+        $helper = $this->getHelper('question');
+        $question =  new Question('Enter B Teams players:');
+        $strTeamB = $helper->ask($input, $output, $question);
+        $this->validateArgument($strTeamB, 'B');
+        if($this->is_valid == false){
+            $errorMessages = array('Error!', $this->error );
+            $formattedBlock = $formatter->formatBlock($errorMessages, 'error');
+            $output->writeln($formattedBlock);
+            return Command::SUCCESS;
+        }
+        if($this->is_valid ==true){
+            foreach($this->team['A'] as $id => $playerStrength){
+                if($this->team['B'][$id] > $playerStrength ){
+                    $this->result = 'Loss';
+                }
+            }
+            $output->writeln($this->result );
+        }
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
-
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
-
+       
         return Command::SUCCESS;
+    }
+    
+    protected function validateArgument(string $argumentInput, string $team):void {
+        $this->error = '';
+        $this->is_valid = true;
+        if( $argumentInput){
+            if(\strpos($argumentInput, ',') === false ){
+                $this->error = 'Player`s of '. $team.' must be comma separeted';
+                $this->is_valid = false;
+            }else{
+                if(!is_numeric(trim(str_replace(',','',$argumentInput)))) {
+                    $this->error = 'Values of '. $team.' must be integer';
+                    $this->is_valid =  false;
+                }else{
+                    $arrTeam = \explode(',', trim($argumentInput));
+
+                    if(count($arrTeam) != 5){
+                        $this->error = 'Player`s of '. $team.' must be 5';
+                        $this->is_valid =  false;
+                    }else{
+                        
+                     $this->team[$team] = $arrTeam;
+                     $this->is_valid =  true;
+                    } 
+                } 
+            }
+        }
     }
 }
